cff67c7b0e4b07eeb4143a9a2e76fbfe5ec0eac6
gabrielesalustinno-cyber <gabrielesalustinno@gmail.com>
2026-02-03 01:05:23 +0000

    criei printf

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,1 +1,59 @@
+#include <stdio.h>
+    int main(){
 
+        //Carta 1
+        char Nome1[50] = "Gabriele";
+        int Idade1 = 22;
+        char Codigo1[50] = "F01 F02 F03 F04 F05 F06 F07";
+        char Estado1[3] = "PB";
+        char Cidade1[50] = "Joao Pessoa";
+        float PIB1 = 75.4;
+        int Populacao1 = 8516870;
+        float Area1 = 56585.0;
+        int PontosTuristicos1 = 15;
+
+    
+    
+        printf("\n=== Carta 1 ===\n");
+
+        printf("\ninforme seu nome: %s\n", Nome1);
+        printf("Idade: %d\n", Idade1);
+        printf("Codigo: %s\n", Codigo1);
+        printf("Estado: %s\n", Estado1);
+        printf("Cidade: %s\n", Cidade1);
+        printf("PIB: %.2f bilhoes de reais\n", PIB1);
+        printf("Populacao: %d\n", Populacao1);
+        printf("Area: %.2f km²\n", Area1);
+        printf("Pontos Turisticos: %d\n", PontosTuristicos1);
+
+
+
+        // Carta 2 //
+
+        char Nome2[50] = "Niuanderson";
+        int Idade2 = 22;
+        char Codigo2[50] = "G01 G02 G03 G04 G05 G06 G07";
+        char Estado2[3] = "PE";
+        char Cidade2[50] = "Recife";
+        float PIB2 = 80.2;
+        int Populacao2 = 9916870;
+        float Area2 = 79885.0;
+        int PontosTuristicos2 = 30;
+
+    
+        printf("\n=== Carta 2 ===\n");
+
+
+        printf("\ninforme seu nome: %s\n", Nome2);
+        printf("Idade: %d\n", Idade2);
+        printf("Codigo: %s\n", Codigo2);
+        printf("Estado: %s\n", Estado2);
+        printf("Cidade: %s\n", Cidade2);
+        printf("PIB: %.2f bilhoes de reais\n", PIB2);
+        printf("Populacao: %d\n", Populacao2);
+        printf("Area: %.2f km²\n", Area2);
+        printf("Pontos Turisticos: %d\n", PontosTuristicos2);
+
+
+    return 0;
+}
